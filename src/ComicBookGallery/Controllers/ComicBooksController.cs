79a7cbb0b4d6d95aac5e2a50266d693dabd541fe
npekhlivanov <nicko@omegasoft.bg>
2018-01-21 23:20:48 +0200

    Added strongly-typed view, models, layout

---
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,31 @@
+﻿using ComicBookGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            var comicBook = new ComicBook()
+            {
+                SeriesTitle = "SpiderMan",
+                IssueNumber = 99,
+                DescriptionHtml = "<p>Final issue!</p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Role = "Script", Name = "John" },
+                    new Artist() { Role = "Pencil", Name = "Jimmy" }
+                }
+            };
+
+            return View(comicBook);
+            //return Content("Hello");
+            //return Redirect("/");
+        }
+    }
+}
